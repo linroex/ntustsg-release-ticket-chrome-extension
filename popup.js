cb6b3c95c99644f267cdce781787e7e6bcf408e6
linroex <linroex@coder.tw>
2013-10-06 21:57:47 +0800

    init

---
+++ popup.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    var reset_switcher = 0;
+    $('#submit').click(function(){
+        search();
+    });
+    $('#reset').click(function(){
+        
+        if(reset_switcher == 1){
+            if($('#passwd').val()=='123456'){
+                chrome.storage.local.set({'stuid-ed':''});
+                $('#passwd').hide();
+                $('#result').html('<div class="notice">已清除記錄</div>');    
+            }else{
+                $('#result').html('<div class="notice">清除專用密碼，輸入錯誤</div>');
+            }
+            
+        }else{
+            $('#passwd').show();
+            reset_switcher = 1;
+            $('#result').html('<div class="notice">輸入密碼後在按一次清除記錄</div>');
+        }
+        
+        
+    });
+})
+function search(){
+    chrome.storage.local.get('stuid-ed',function(e){
+        if(e['stuid-ed']==undefined){
+            var stuid = "";
+        }else{
+            var stuid = e['stuid-ed'];    
+        }
+        if($('#stuid').val()==''){
+            $('#result').html('<span style="font-size:35px">請輸入學號</span>');
+        }else if(stuid.indexOf($('#stuid').val().toLowerCase())!=-1){
+            $('#result').html('<div class="result-id">' + $('#stuid').val().toUpperCase() + '</div>已領票');
+        }else{
+            $('#result').html('<div class="result-id">' + $('#stuid').val().toUpperCase() + '</div>未領票');
+            chrome.storage.local.set({'stuid-ed':stuid + ";" + $('#stuid').val().toLowerCase()});
+        }
+        $('#stuid').val('');
+    })
+    
+}
